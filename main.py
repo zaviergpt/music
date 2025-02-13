417b401707f5aae8030d6ff7a2323595aff1b5ce
Zavier Chiam <60832719+zaviergpt@users.noreply.github.com>
2025-02-13 21:45:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import time
 import sys
 import tinytag
 import datetime
+import csv
 
 os.system("cls")
 
@@ -17,7 +18,7 @@ class Audio:
 
     def signature(self, length=2**3):
         data = self.data.copy()
-        characters = "aA0bB1cC2dD3eE4fF5gG6hH7iI8jJ9kK0lL1mM2nN3oO4pP5qQ6rR7sS8tT9uU0vV1wW2xX3yY4zZ5"
+        characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
         if data.ndim == 1:
             data = np.column_stack((data, data))
         data = np.abs(data.astype(np.float32)) / np.iinfo(np.int16).max
@@ -68,9 +69,7 @@ class Audio:
 def log(text):
     sys.stdout.write("\0337")
     sys.stdout.write(f"\033[{size.lines};{1}H")
-    sys.stdout.write(u"\u001b[2K\u001b[0m\u001b[1000D\u001b[1m\u001b[37;1m")
-    sys.stdout.write("Track {}/{}".format(index+1, len(audiofiles)))
-    sys.stdout.write(u"\u001b[31;1m ")
+    sys.stdout.write(u"\u001b[2K\u001b[0m\u001b[1000D\u001b[1m\u001b[31;1m")
     sys.stdout.write("{}".format(str(datetime.timedelta(seconds=round(((time.time() - start)/(index+1)) * (len(audiofiles) - (index+1)))))))
     sys.stdout.write(u"\u001b[37;1m ")
     sys.stdout.write("{}".format(file.split("\\")[-1]))
@@ -81,27 +80,44 @@ def log(text):
     
 if __name__ in "__main__":
     audiofiles = []
+    ids = []
     for root, dirs, files in os.walk(os.getcwd()):
         for file in files:
             if file.endswith(".flac"):
                 audiofiles.append(os.path.join(root, file))
     start = time.time()
     size = os.get_terminal_size()
+    writer = csv.writer(open("tracks.csv", "w", newline=""))
     sys.stdout.write(f"\033[{1};{size.lines-1}r")
     sys.stdout.write(f"\033[{1};{1}H")
     sys.stdout.flush()
     for index, file in enumerate(audiofiles):
         audio = Audio(file)
-        log(u"\u001b[37;1m{} {}\u001b[0m".format(index+1, file.split("\\")[-1]))
-        log("\nretriving signature ... ")
         signature = audio.signature()
-        log(signature)
-        log("\n\n")
+        metadata = {
+            "title": audio.metadata["title"][0],
+            "album": audio.metadata["album"][0],
+            # "artists": audio.metadata["artist"],
+            # "albumartist": audio.metadata["albumartist"] if audio.metadata["albumartist"][0] in audio.metadata["artist"] else "Various Artists",
+            "mainartist": [artist for artist in audio.metadata["albumartist"] if artist in audio.metadata["artist"]][0] if len([artist for artist in audio.metadata["albumartist"] if artist in audio.metadata["artist"]]) > 0 else audio.metadata["artist"][0]
+        }
+        if index < 1:
+            writer.writerow([item[0] for item in metadata.items()])
+        if signature not in ids:
+            writer.writerow([item[1] for item in metadata.items()])
+            log(u"\u001b[32;1m{}".format(signature))
+            ids.append(signature)
+        else:
+            log(u"\u001b[31;1m{}".format(signature))
+        log(" {}".format(file.split("\\")[-1]))
+        log("\u001b[37;1m {} - {}".format(metadata["title"], metadata["mainartist"]))
+        log("\n")
 
 sys.stdout.write("\0337")
+
 sys.stdout.write(f"\033[{size.lines};{1}H")
 sys.stdout.write(u"\u001b[2K\u001b[0m\u001b[1000D\u001b[1m\u001b[37;1m")
 sys.stdout.write(u"\u001b[0m ")
 sys.stdout.write("\0338")
-sys.stdout.write("Finished Successfully.\n\n")
+sys.stdout.write("\nFinished Successfully.\n\n")
 sys.stdout.flush()
